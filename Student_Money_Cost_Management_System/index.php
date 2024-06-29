8d2b6fbb738c72f93e35c9fc18099384c37358de
imrancaptain <imranicon01@gmail.com>
2024-06-29 23:09:20 +0600

    Add files via upload

---
+++ Student_Money_Cost_Management_System/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h2> Student Money Cost Management System </h2>
+        <fieldset>
+
+        <form action="Process.php" method="post">
+       
+       
+            ID : <input type="text"  name="student"><br>
+            
+            Name : <input type="text"  name="name"><br>
+            Em@il : <input type="text"  name="email"><br>
+            <input type="submit"  value="Submit">
+            <a href="home.php" class="button" value = "back">back</a>
+            
+         
+            
+
+
+        </form>
+        
+</fieldset>
+    </body>
+</html>
---
+++ Student_Money_Cost_Management_System/shopping.html
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+<h2>Stuent Money Cost Management System</h1>
+    <h1>Welcome to the Home Page</h1>
+    <p>Click the link below to go to the About Page:</p>
+    <a href="index.php"> About</a>
+    <a href="costdetails.html">Cost Details</a>
+    
+    <a href="account.html">Accounts</a>
+</body>
+</html>
